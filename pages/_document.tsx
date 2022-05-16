426e0281757aef8daa65b25832bde23667eb847d
HraZak <hrazak.hz@gmail.com>
2022-05-16 13:06:16 +0200

    Přidán _document

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx);
+  }
+
+  render() {
+    return (
+      <Html lang='cs'>
+        <Head>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
