0e2acb8b88f54c267c6824d9d84dcdf697f8b5c3
HraZak <hrazak.hz@gmail.com>
2022-05-16 12:30:24 +0200

    Přesunutí DivLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,43 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { DivButton, DivDisplay, DivGrid, DivLayout } from '../components';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <DivLayout>
       <Head>
         <title>Kalkulačka</title>
         <meta name='description' content='Kalkulačka' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <DivLayout>
-        <DivGrid>
-          <DivDisplay></DivDisplay>
+      <DivGrid>
+        <DivDisplay></DivDisplay>
 
-          <DivButton pozice='ac'>AC</DivButton>
-          <DivButton pozice='del'>DEL</DivButton>
-          <DivButton pozice='deleno'>/</DivButton>
+        <DivButton pozice='ac'>AC</DivButton>
+        <DivButton pozice='del'>DEL</DivButton>
+        <DivButton pozice='deleno'>/</DivButton>
 
-          <DivButton pozice='sedm'>7</DivButton>
-          <DivButton pozice='osm'>8</DivButton>
-          <DivButton pozice='devet'>9</DivButton>
-          <DivButton pozice='krat'>*</DivButton>
+        <DivButton pozice='sedm'>7</DivButton>
+        <DivButton pozice='osm'>8</DivButton>
+        <DivButton pozice='devet'>9</DivButton>
+        <DivButton pozice='krat'>*</DivButton>
 
-          <DivButton pozice='ctyri'>4</DivButton>
-          <DivButton pozice='pet'>5</DivButton>
-          <DivButton pozice='sest'>6</DivButton>
-          <DivButton pozice='plus'>+</DivButton>
+        <DivButton pozice='ctyri'>4</DivButton>
+        <DivButton pozice='pet'>5</DivButton>
+        <DivButton pozice='sest'>6</DivButton>
+        <DivButton pozice='plus'>+</DivButton>
 
-          <DivButton pozice='jedna'>1</DivButton>
-          <DivButton pozice='dva'>2</DivButton>
-          <DivButton pozice='tri'>3</DivButton>
-          <DivButton pozice='minus'>-</DivButton>
+        <DivButton pozice='jedna'>1</DivButton>
+        <DivButton pozice='dva'>2</DivButton>
+        <DivButton pozice='tri'>3</DivButton>
+        <DivButton pozice='minus'>-</DivButton>
 
-          <DivButton pozice='tecka'>.</DivButton>
-          <DivButton pozice='nula'>0</DivButton>
-          <DivButton pozice='rovno'>=</DivButton>
-        </DivGrid>
-      </DivLayout>
-    </div>
+        <DivButton pozice='tecka'>.</DivButton>
+        <DivButton pozice='nula'>0</DivButton>
+        <DivButton pozice='rovno'>=</DivButton>
+      </DivGrid>
+    </DivLayout>
   );
 };
 
