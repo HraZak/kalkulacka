5714fea6e57f6ba95e30747973dd14d11d9f9539
HraZak <hrazak.hz@gmail.com>
2022-05-16 12:18:10 +0200

    Použití komponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styled from 'styled-components';
+import { DivButton, DivDisplay, DivGrid, DivLayout } from '../components';
 import styles from '../styles/Home.module.css';
-
-const Layout = styled.div``;
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +12,34 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      <DivLayout>
+        <DivGrid>
+          <DivDisplay></DivDisplay>
+
+          <DivButton pozice='ac'>AC</DivButton>
+          <DivButton pozice='del'>DEL</DivButton>
+          <DivButton pozice='deleno'>/</DivButton>
+
+          <DivButton pozice='sedm'>7</DivButton>
+          <DivButton pozice='osm'>8</DivButton>
+          <DivButton pozice='devet'>9</DivButton>
+          <DivButton pozice='krat'>*</DivButton>
+
+          <DivButton pozice='ctyri'>4</DivButton>
+          <DivButton pozice='pet'>5</DivButton>
+          <DivButton pozice='sest'>6</DivButton>
+          <DivButton pozice='plus'>+</DivButton>
+
+          <DivButton pozice='jedna'>1</DivButton>
+          <DivButton pozice='dva'>2</DivButton>
+          <DivButton pozice='tri'>3</DivButton>
+          <DivButton pozice='minus'>-</DivButton>
+
+          <DivButton pozice='tecka'>.</DivButton>
+          <DivButton pozice='nula'>0</DivButton>
+          <DivButton pozice='rovno'>=</DivButton>
+        </DivGrid>
+      </DivLayout>
     </div>
   );
 };
