a0ec33a25339c27123f27d25cf789252154fc80d
HraZak <hrazak.hz@gmail.com>
2022-05-24 13:11:30 +0200

    Vytvoření funkce pridatZavorku

--- components/functions.ts
+++ components/functions.ts
@@ -1,3 +1,5 @@
+import { operatory } from '../constants/math_const';
+
 export const arrayPosledni = (array: string[]) => {
   return array[array.length - 1] ?? '';
 };
@@ -10,4 +12,30 @@ export const smazat = (array: string[]) => {
       i < array_temp.length - 1 ? e : e.slice(0, -1),
     );
   else return array_temp.filter((e, i) => i < array_temp.length - 1);
+};
+
+export const pridatZavorku = (array: string[], x: string) => {
+  const array_temp = [...array];
+  const zadanoPosledni = arrayPosledni(array_temp);
+
+  switch (x) {
+    case '(':
+      if (
+        operatory.includes(zadanoPosledni) ||
+        zadanoPosledni === '' ||
+        zadanoPosledni === '('
+      )
+        return [...array_temp, x];
+      break;
+    case ')':
+      if (
+        !operatory.includes(zadanoPosledni) &&
+        zadanoPosledni !== '(' &&
+        zadanoPosledni !== ''
+      )
+        return [...array_temp, x];
+      break;
+  }
+
+  return array_temp;
 };
